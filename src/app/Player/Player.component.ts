397162c3485ba35c6aa8626804c73faec34ace8c
Peter Boomsma <peter@cleanpixel.nl>
2024-07-08 14:04:08 +0200

    Initialized game

---
+++ src/app/Player/Player.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  template: '',
+  styleUrls: ['./Player.component.css'],
+  standalone: true
+})
+export class PlayerComponent {
+}
